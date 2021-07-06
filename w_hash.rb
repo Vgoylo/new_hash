c7a8e0b4c7aaeeb88a4f18b9e15aa91681894ff4
vgoylo <y_draiv.by@mail.ru>
2021-07-06 15:10:32 +0300

    Fix errors

--- w_hash.rb
+++ w_hash.rb
@@ -3,14 +3,15 @@
     "The Batman" => "Bruce Wayne 1",
     "Superman" => "Clark Kent",
     "Wonder Woman" => "Diana Prince",
-    "Freakazoid" => "Dexter Douglas"
-
+    "Freakazoid" => "Dexter Douglas",
+    'aaa' => 21,
+    [1212, 434, 'dad',] => {"fgfgfg" => 12121, 33331 => 'a'} 
   }
 def secret_identities(hash)
   result = {}
 
   hash.each do |key,value| 
-    if key.include?('a') && value.include?('1')
+    if key.to_s.include?('a') && value.to_s.include?('1')
       result[key] = value
     end
   end
